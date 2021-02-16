f6975739eca3c8db84957dd43e9b5216e835f1f8
Navin Hewadewa <theusuallima@gmail.com>
2021-02-16 21:31:46 +0000

    navigation implemented, activities -> fragments

---
+++ app/src/main/java/com/jason/daisy/scrambletimer/ScrambleTimerFragment.kt
@@ -0,0 +1,102 @@
+package com.jason.daisy.scrambletimer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.jason.daisy.R
+import com.jason.daisy.databinding.FragmentScrambleTimerBinding
+
+class ScrambleTimerFragment : Fragment(), AdapterView.OnItemSelectedListener {
+    private var _binding: FragmentScrambleTimerBinding? = null
+    private val binding: FragmentScrambleTimerBinding
+        get() = requireNotNull(_binding)
+    private lateinit var viewModel : ScrambleTimerViewModel
+    private lateinit var hiddenElements : List<View>
+
+    companion object {
+        fun newInstance() = ScrambleTimerFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentScrambleTimerBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Elements to hide when solving
+        hiddenElements = listOf(binding.lunaButton,
+            binding.viewSolvesButton,
+            binding.puzzleSpinner,
+            binding.scrambleTextView
+        )
+
+        //Get ViewModel reference
+        viewModel = ViewModelProvider(this, ScrambleTimerViewModelFactory(requireActivity().application)).get(
+            ScrambleTimerViewModel::class.java)
+
+        //Update Spinner with puzzle types
+        binding.puzzleSpinner.adapter = ArrayAdapter(requireActivity(), R.layout.support_simple_spinner_dropdown_item, PuzzleType.values())
+        binding.puzzleSpinner.onItemSelectedListener = this
+
+        //Attach observers to livedata
+        viewModel.currentTime.observe(requireActivity(), { binding.timerTextView.text = it })
+        viewModel.timerColor.observe(requireActivity(), { binding.timerTextView.setTextColor(it) })
+        viewModel.scramble.observe(requireActivity(), { binding.scrambleTextView.text = it })
+        viewModel.timerActive.observe(requireActivity(), { setElementsVisibility(hiddenElements, !it) })
+
+        //Listener setup
+        binding.lunaButton.setOnClickListener {
+            Toast.makeText(binding.lunaButton.context, "I Love you, Luna", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.viewSolvesButton.setOnClickListener {
+            changeScreen()
+        }
+
+        binding.mainLayout.setOnTouchListener { _, event ->
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> !viewModel.handleActionDown()
+                MotionEvent.ACTION_UP -> {
+                    binding.mainLayout.performClick()
+                    viewModel.handleActionUp()
+                    false
+                }
+                else -> true
+            }
+        }
+    }
+
+    private fun setElementsVisibility(list: List<View>, setVisible: Boolean) {
+        for(e in list) {
+            e.visibility = if(setVisible) View.VISIBLE else View.INVISIBLE
+        }
+    }
+
+    private fun changeScreen() {
+        val action = ScrambleTimerFragmentDirections.actionScrambleTimerFragmentToViewSolvesFragment()
+        findNavController().navigate(action)
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        val puzzleChosen = PuzzleType.getPuzzleType(parent.getItemAtPosition(pos).toString())
+        viewModel.changePuzzle(puzzleChosen)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/jason/daisy/viewsolves/ViewSolvesFragment.kt
@@ -0,0 +1,85 @@
+package com.jason.daisy.viewsolves
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jason.daisy.database.Solve
+import com.jason.daisy.databinding.FragmentViewSolvesBinding
+
+class ViewSolvesFragment : Fragment(), SolvesAdapterListener {
+    private var _binding: FragmentViewSolvesBinding? = null
+    private val binding: FragmentViewSolvesBinding
+        get() = requireNotNull(_binding)
+    private lateinit var viewModel: ViewSolvesViewModel
+    private lateinit var adapter: SolvesAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreate(savedInstanceState)
+        _binding = FragmentViewSolvesBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val puzzleTypeString = "Three by Three"
+        viewModel = ViewModelProvider(this, ViewSolvesViewModelFactory(requireActivity().application, puzzleTypeString)).get(ViewSolvesViewModel::class.java)
+        setUpRecycler()
+        binding.viewTimerButton.setOnClickListener { changeScreen() }
+        binding.deleteAllButton.setOnClickListener { showDeleteAllDialog() }
+
+        viewModel.data.observe(viewLifecycleOwner, { adapter.submitList(it) })
+    }
+
+    private fun setUpRecycler() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        adapter = SolvesAdapter(this)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.setHasFixedSize(true)
+
+        val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback(adapter))
+        itemTouchHelper.attachToRecyclerView(binding.recyclerView)
+    }
+
+    private fun changeScreen() {
+        val action = ViewSolvesFragmentDirections.actionViewSolvesFragmentToScrambleTimerFragment()
+        findNavController().navigate(action)
+    }
+
+    override fun deleteSolve(s: Solve) {
+        viewModel.delete(s)
+    }
+
+    private fun showDeleteAllDialog() {
+        lateinit var dialog: AlertDialog
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Delete All Solves")
+        builder.setMessage("This action will delete all solves. Continue?")
+        val dialogClickListener = DialogInterface.OnClickListener { _, which ->
+            when (which) {
+                DialogInterface.BUTTON_POSITIVE -> {
+                    viewModel.deleteAll()
+                }
+                else -> Log.d("DELETEALLSOLVES", "CANCELLED")
+            }
+        }
+        builder.setPositiveButton("YES", dialogClickListener)
+        builder.setNegativeButton("NO", dialogClickListener)
+
+        dialog = builder.create()
+        dialog.show()
+    }
+}
